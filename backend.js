ed24de51e673c8601febc7712387f06a0e81b4a5
simantaray <simantaray89@gmail.com>
2022-02-18 20:21:28 +0530

    add : cors

--- backend.js
+++ backend.js
@@ -3,32 +3,35 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 
-let array = fs.readFileSync("links.txt", "utf8").split("\n");
+app.get("/links", (req, res) => {
 
-let subreddit = array[Math.floor(Math.random() * 21)];
+  let array = fs.readFileSync("links.txt", "utf8").split("\n");
 
-let subredditLink = `https://www.reddit.com/${subreddit}`;
+  let subreddit = array[Math.floor(Math.random() * 21)];
 
-console.log(subredditLink);
+  let subredditLink = `https://www.reddit.com/${subreddit}`;
 
-app.get("/links",(req,res)=>{
+  console.log(`connecting to ${subredditLink}`);
   (async () => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     await page.goto(subredditLink);
-  
+
     await page.waitForSelector(
       'button[class="i2sTp1duDdXdwoKi1l8ED _2iuoyPiKHN3kfOoeIQalDT _2tU8R9NTqhvBrhoNAXWWcP HNozj_dKjQZ59ZsfEegz8 _2nelDm85zKKmuD94NequP0"]'
     );
-  
+
     await page.click(
       'button[class="i2sTp1duDdXdwoKi1l8ED _2iuoyPiKHN3kfOoeIQalDT _2tU8R9NTqhvBrhoNAXWWcP HNozj_dKjQZ59ZsfEegz8 _2nelDm85zKKmuD94NequP0"]'
     );
-  
+
     await page.waitForSelector("body");
-  
+
     await page.goto(subredditLink, {
       waitUntil: "networkidle0",
     });
@@ -42,6 +45,6 @@ app.get("/links",(req,res)=>{
     console.log(links);
     res.status(200).json(links);
   })();
-})
+});
 
 app.listen(3000, () => console.log("listing to 3000"));
--- index.js
+++ index.js
@@ -1,75 +1,75 @@
-'use strict';
-const https = require('https');
-const puppeteer = require("puppeteer");
-const fs = require("fs");
-const path = require("path");
+// 'use strict';
+// const https = require('https');
+// const puppeteer = require("puppeteer");
+// const fs = require("fs");
+// const path = require("path");
 
 
-let array = fs.readFileSync('links.txt', 'utf8').split('\n');
+// let array = fs.readFileSync('links.txt', 'utf8').split('\n');
 
-let subreddit=array[Math.floor(Math.random() * 21)]
+// let subreddit=array[Math.floor(Math.random() * 21)]
 
-let subredditLink=`https://www.reddit.com/${subreddit}`
+// let subredditLink=`https://www.reddit.com/${subreddit}`
 
-console.log(subredditLink);
+// console.log(subredditLink);
 
-//puppeteer
+// //puppeteer
 
-(async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(subredditLink);
+// (async () => {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
+//   await page.goto(subredditLink);
 
-  await page.waitForSelector(
-    'button[class="i2sTp1duDdXdwoKi1l8ED _2iuoyPiKHN3kfOoeIQalDT _2tU8R9NTqhvBrhoNAXWWcP HNozj_dKjQZ59ZsfEegz8 _2nelDm85zKKmuD94NequP0"]'
-  );
+//   await page.waitForSelector(
+//     'button[class="i2sTp1duDdXdwoKi1l8ED _2iuoyPiKHN3kfOoeIQalDT _2tU8R9NTqhvBrhoNAXWWcP HNozj_dKjQZ59ZsfEegz8 _2nelDm85zKKmuD94NequP0"]'
+//   );
 
-  await page.click(
-    'button[class="i2sTp1duDdXdwoKi1l8ED _2iuoyPiKHN3kfOoeIQalDT _2tU8R9NTqhvBrhoNAXWWcP HNozj_dKjQZ59ZsfEegz8 _2nelDm85zKKmuD94NequP0"]'
-  );
+//   await page.click(
+//     'button[class="i2sTp1duDdXdwoKi1l8ED _2iuoyPiKHN3kfOoeIQalDT _2tU8R9NTqhvBrhoNAXWWcP HNozj_dKjQZ59ZsfEegz8 _2nelDm85zKKmuD94NequP0"]'
+//   );
 
-  await page.waitForSelector("body");
+//   await page.waitForSelector("body");
 
-  await page.goto(subredditLink, {
-    waitUntil: "networkidle0",
-  });
-  const links = await page.evaluate(async () => {
-    window.scrollBy(0, document.body.clientHeight);
-    await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for some time, you might need to figure out a good value for this yourself
-    // return [...document.querySelectorAll('.scrollerItem div:nth-of-type(2) article div:nth-of-type(2) div:nth-of-type(2) a')]
-    return [...document.querySelectorAll(".ImageBox-image")].map(
-      (el) => el.src
-    );
-  });
-  console.log(links);
-  for (let i = 0; i < links.length; i++) {
-   let result = await download(links[i], `image-${i}.png`);
+//   await page.goto(subredditLink, {
+//     waitUntil: "networkidle0",
+//   });
+//   const links = await page.evaluate(async () => {
+//     window.scrollBy(0, document.body.clientHeight);
+//     await new Promise((resolve) => setTimeout(resolve, 5000)); // wait for some time, you might need to figure out a good value for this yourself
+//     // return [...document.querySelectorAll('.scrollerItem div:nth-of-type(2) article div:nth-of-type(2) div:nth-of-type(2) a')]
+//     return [...document.querySelectorAll(".ImageBox-image")].map(
+//       (el) => el.src
+//     );
+//   });
+//   console.log(links);
+//   for (let i = 0; i < links.length; i++) {
+//    let result = await download(links[i], `image-${i}.png`);
 
-    if (result === true) {
-      console.log("Success:", links[i], "has been downloaded successfully.");
-    } else {
-      console.log("Error:", links[i], "was not downloaded.");
-      console.error(result);
-    }
-  }
-})();
+//     if (result === true) {
+//       console.log("Success:", links[i], "has been downloaded successfully.");
+//     } else {
+//       console.log("Error:", links[i], "was not downloaded.");
+//       console.error(result);
+//     }
+//   }
+// })();
 
-const download = (url, destination) =>
-  new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(destination);
+// const download = (url, destination) =>
+//   new Promise((resolve, reject) => {
+//     const file = fs.createWriteStream(destination);
 
-    https
-      .get(url, (response) => {
-        response.pipe(file);
+//     https
+//       .get(url, (response) => {
+//         response.pipe(file);
 
-        file.on("finish", () => {
-          file.close(resolve(true));
-        });
-      })
-      .on("error", (error) => {
-        fs.unlink(destination);
+//         file.on("finish", () => {
+//           file.close(resolve(true));
+//         });
+//       })
+//       .on("error", (error) => {
+//         fs.unlink(destination);
 
-        reject(error.message);
-      });
-  });
+//         reject(error.message);
+//       });
+//   });
 
