a8f759c6a19474d1da5a62ec4a301b10c660742c
simantaray <simantaray89@gmail.com>
2022-02-18 20:29:07 +0530

    remove: unnecessary files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
- 
-const desplay= document.getElementById("main").innerHTML;
+//
+let display=document.getElementById("main");
 
-fetch('http://localhost:3000')
+fetch('http://localhost:3000/links')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(links => {
+    links.forEach(link=>{
+      console.log(link);
+      display.innerHTML += `<img src=${link}>`;
+    })
+  });
